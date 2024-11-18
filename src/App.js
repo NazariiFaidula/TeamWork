706ffde3daaa59dab8a769a61a7eddf330211a40
NazariiFaidula <nazarii.faidula.pz.2023@lpnu.ua>
2024-11-18 20:31:24 +0200

    Added Nazarii card

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import YavorskyiImage from './Yavorskyi.jpg';
 import MaksImage from './Photo.jpg';
 import ShevchukDmytro from './ShevchukDmytro.jpg';
+import NazariiImage from './Nazarii.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="container">
         <div className="row">
+            <div className="card">
+                <img src={NazariiImage}/>
+                <h2>Назарій Н.А.</h2>
+                <p>Project Manager</p>
+            </div>
             <div className="card">
                 <img src={MaksImage}/>
                 <h2>Фалібога М.С.</h2>
