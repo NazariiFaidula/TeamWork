aaed25df990419023c7f3e4ea60b5745a1bc7742
Fir3ice <andrii.yavorskyi.pz.2023@lpnu.ua>
2024-11-18 19:44:13 +0200

    Andrii Yavorskyi data on website

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import NazariiImage from './NazariiFaidula.jpg';
+import YavorskyiImage from './Yavorslyi.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="container">
         <div className="row">
-            <div className="card">
-                <img src={NazariiImage}/>
-                <h2>Файдула Н.А.</h2>
-                <p>Project Manager</p>
-            </div>
         </div>
         <div className="row">
+		<div className="card">
+                <img src={YavorskyiImage}/>
+                <h2>Яворський А.В.</h2>
+                <p>UI/UX designer</p>
+            </div>
         </div>
     </div>
   );
