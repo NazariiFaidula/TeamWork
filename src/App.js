fca7092d2e11989f39d5944240a73095a46ca826
NazariiFaidula <nazarii.faidula.pz.2023@lpnu.ua>
2024-11-18 17:41:34 +0200

    Added Nazarii card

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <div className="box">
-        Hello
-      </div>
+    <div className="container">
+        <div className="row">
+            <div className="card">
+                <img src="49.jpg"></img>
+                <h2>Файдула Н.А.</h2>
+                <p>Project Manager</p>
+            </div>
+        </div>
+        <div className="row">
+        </div>
     </div>
   );
 }
